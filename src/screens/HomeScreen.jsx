c7bd21ec3ed6c2e40dbe1f694be8d8a3d7ec1661
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-04-25 11:33:14 +0100

    Updated

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,8 +2,16 @@ import '../styles/home.css'
 
 function HomeScreen() {
     return (
-        <div>
+        <div className='top'>
             <h1>VINTAGEFUR</h1>
+            <ul>
+                <li>home</li>
+                <li>servicess</li>
+                <li>about</li>
+                <li>shop</li>
+                <li>contact</li>
+            </ul>
+            <h2>LOGIN</h2>
         </div>
     )
 }
